7b7822c851e7a8c1f4cf05f404ed5c257835a51c
kimsejeong <clean01@hanyang.ac.kr>
2024-01-27 23:52:26 +0900

    feat: SecurityConfig, AccountService 수정

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -1,22 +1,57 @@
 package com.shop.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity.*;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity // 이거 지우니까 안뜬다
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public WebSecurityCustomizer wedSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(String.valueOf(PathRequest.toStaticResources().atCommonLocations()));
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/thymeleaf/ex07", "/members/login", "/memebers/new").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/members/login")
+                        .defaultSuccessUrl("/", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .permitAll());
+
+        return http.build();
+    }
+
+
+
 
 }
---
+++ src/main/java/com/shop/service/AccountService.java
@@ -0,0 +1,48 @@
+package com.shop.service;
+
+import com.shop.entity.Member;
+import com.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder encoder;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email);
+
+        if(member == null) {
+            throw new UsernameNotFoundException(email);
+        }
+
+        // UserDetail을 구현하고 있는 객체 반환
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
+    }
+
+    @Transactional
+    public boolean join(String userName, String userEmail, String userPwd) {
+
+        if(memberRepository.findByEmail(userEmail) != null) {
+            return false;
+        }
+
+
+    }
+
+}
